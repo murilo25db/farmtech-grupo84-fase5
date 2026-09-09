7a9f109a11f1541328266bccb00334608d3e42c1
murilo25db <murilodb25@gmail.com>
2026-09-08 23:39:08 -0300

    Arquivo de apoio: config.example.py
    Arquivo de apoio da entrega.

---
+++ config.example.py
@@ -0,0 +1,6 @@
+WIFI_SSID = 'NOME_DA_REDE'
+WIFI_SENHA = 'PREENCHER_LOCALMENTE'
+API_URL = 'http://192.168.1.100:8000'  # Troque pelo IP do PC na rede de laboratório.
+DISPOSITIVO = 'esp32-canteiro-01'
+ADC_SECO = 3200     # Exemplo de ponto inicial: substitua pela sua medição.
+ADC_MOLHADO = 1400  # Exemplo de ponto inicial: substitua pela sua medição.
